b7127cb56cace45ea712112775ec3acc46959021
ericmunozs <eric.munozsoldado@gmail.com>
2021-04-02 11:34:53 +0200

    Added project title. Header added and footer removed (structure modified)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import Footer from './Footer'
+import Header from './Header'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
+import { Typography } from '@material-ui/core'
 
 const App = () => (
   <div>
+    <Typography variant="h3"><b>Todo App</b></Typography>
+    <Header />
     <VisibleTodoList />
     <AddTodo />
-    <Footer />
   </div>
 )
 
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import FilterLink from "../containers/FilterLink";
+
+// Styles
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    ...theme.typography.button,
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(1),
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <b>Mostrar: </b>
+      <FilterLink filter="SHOW_ALL">Todos</FilterLink>
+      {", "}
+      <FilterLink filter="SHOW_ACTIVE">Activos</FilterLink>
+      {", "}
+      <FilterLink filter="SHOW_COMPLETED">Completados</FilterLink>
+    </div>
+  );
+};
+export default Header;
